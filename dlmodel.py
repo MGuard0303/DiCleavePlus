070aafbe87f8430d16673524d7d270156e3245a6
Mu <mulixuan1994@gmail.com>
2025-07-15 20:45:58 +0900

    Update codes

--- dlmodel.py
+++ dlmodel.py
@@ -591,40 +591,6 @@ class ModelConcatFlex(nn.Module):
         return embed
 
 
-class DCAE(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.name = "DCAE"
-
-        self.encoder = nn.Sequential(
-            nn.BatchNorm1d(800),
-            nn.Linear(800, 512),
-            nn.ReLU(),
-            nn.BatchNorm1d(512),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.BatchNorm1d(256),
-            nn.Linear(256, 64),
-            nn.ReLU()
-        )
-
-        self.decoder = nn.Sequential(
-            nn.Linear(64, 256),
-            nn.ReLU(),
-            nn.Linear(256, 512),
-            nn.ReLU(),
-            nn.Linear(512, 800),
-            nn.ReLU()
-        )
-
-    def forward(self, x):
-        x = x.reshape(-1, 800)
-        encoder_out = self.encoder(x)
-        decoder_out = self.decoder(encoder_out)
-
-        return encoder_out, decoder_out, x
-
-
 class DC(nn.Module):
     def __init__(self, pattern_size):
 
