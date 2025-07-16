b27cc6fcf6e3cffb37af142f8140d4056a5f2130
MGuard1994 <53183964+MGuard1994@users.noreply.github.com>
2025-07-16 11:08:07 +0900

    Update codes

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ import utils
 # Hyper parameters.
 date = datetime.datetime.now().strftime("%Y%m%d")
 task = "aff_f_14_2"  # "model type, pattern size, dataset type".
-expt_no = 7
+expt_no = 1
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 pattern_size = 14
 epoch_size = 100
@@ -108,10 +108,10 @@ for fold in range(1, 6):
         num_tf_layer=3,
         linear_hidden_feature=64,
     )
-    # loss_fn_weight = torch.ones(pattern_size)
-    # loss_fn_weight[0] = 0.1
-    # model.loss_function = torch.nn.NLLLoss(weight=loss_fn_weight.to(device))
-    model.loss_function = torch.nn.NLLLoss()
+    loss_fn_weight = torch.ones(pattern_size)
+    loss_fn_weight[0] = 0.5
+    model.loss_function = torch.nn.NLLLoss(weight=loss_fn_weight.to(device))
+    # model.loss_function = torch.nn.NLLLoss()
     model.optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, weight_decay=1e-5)
 
     model.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
